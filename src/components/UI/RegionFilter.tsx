b8e4e2d8f860660293fa189a87e779df83b19af3
Lorand98 <kalmarlorand@yahoo.com>
2022-04-11 23:36:56 +0300

    RegionFilter component created. TODO: include it in the <App/> and create the style

---
+++ src/components/UI/RegionFilter.tsx
@@ -0,0 +1,16 @@
+const RegionFilter: React.FC = () => {
+  return (
+    <div>
+      <div>Filter By Region</div>
+      <ul>
+        <li>Africa</li>
+        <li>America</li>
+        <li>Asia</li>
+        <li>Europe</li>
+        <li>Oceania</li>
+      </ul>
+    </div>
+  );
+};
+
+export default RegionFilter;
