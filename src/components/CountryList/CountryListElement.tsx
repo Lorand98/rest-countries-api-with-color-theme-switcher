d5ce3cc5fbb9c39a4152c9219a33e890d70554c1
Lorand Kalmar <Lorand.Kalmar@wkfs-frc.local>
2022-04-20 16:54:03 +0300

    Skeleton animation created with initial placeholder divs

--- src/components/CountryList/CountryListElement.tsx
+++ src/components/CountryList/CountryListElement.tsx
@@ -1,60 +1,67 @@
-import { useContext } from 'react';
-import { ThemeContext } from '../../context/theme-context';
-import { Country } from '../../types';
+import { useContext } from "react";
+import { ThemeContext } from "../../context/theme-context";
+import { Country } from "../../types";
 
-import classes from './CountryListElement.module.scss';
+import classes from "./CountryListElement.module.scss";
 
-const CountryListElement: React.FC<{ country: Country }> = ({ country }) => {
+const CountryListElement: React.FC<{ country?: Country }> = ({ country }) => {
   const themeCtx = useContext(ThemeContext);
   const countryThemeClass = themeCtx.isLight
-    ? classes['country--light']
-    : classes['country--dark'];
+    ? classes["country--light"]
+    : classes["country--dark"];
+
+  if (!country) {
+    return (
+      <li className={[classes["country"], countryThemeClass].join(" ")}>
+        <div className={classes["skeleton"]}></div>
+        <div className={classes["skeleton skeleton-text"]}></div>
+        <div className={classes["skeleton skeleton-text"]}></div>
+      </li>
+    );
+  }
 
   return (
-    <li
-      key={country.cca2}
-      className={[classes['country'], countryThemeClass].join(' ')}
-    >
+    <li className={[classes["country"], countryThemeClass].join(" ")}>
       <img
         alt={`${country.name.common} flag`}
         src={country.flags.svg}
-        className={classes['country__flag']}
+        className={classes["country__flag"]}
       />
-      <div className={classes['country__description']}>
-        <div className={classes['country__description__name-wrapper']}>
-          <h1 className={classes['country__description__name']}>
+      <div className={classes["country__description"]}>
+        <div className={classes["country__description__name-wrapper"]}>
+          <h1 className={classes["country__description__name"]}>
             {country.name.common}
           </h1>
         </div>
-        <div className={classes['country__description__details']}>
-          <p className={classes['country__description__details__property']}>
+        <div className={classes["country__description__details"]}>
+          <p className={classes["country__description__details__property"]}>
             <span
               className={
-                classes['country__description__details__property__name']
+                classes["country__description__details__property__name"]
               }
             >
               Population:
-            </span>{' '}
-            {country.population.toLocaleString('en-US')}
+            </span>{" "}
+            {country.population.toLocaleString("en-US")}
           </p>
-          <p className={classes['country__description__details__property']}>
+          <p className={classes["country__description__details__property"]}>
             <span
               className={
-                classes['country__description__details__property__name']
+                classes["country__description__details__property__name"]
               }
             >
               Region:
-            </span>{' '}
+            </span>{" "}
             {country.region}
           </p>
-          <p className={classes['country__description__details__property']}>
+          <p className={classes["country__description__details__property"]}>
             <span
               className={
-                classes['country__description__details__property__name']
+                classes["country__description__details__property__name"]
               }
             >
               Capital:
-            </span>{' '}
+            </span>{" "}
             {country.capital}
           </p>
         </div>
