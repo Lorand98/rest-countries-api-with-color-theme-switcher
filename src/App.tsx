6221131fbbb878df80843d96928a1547d3b1f847
Lorand Kalmar <Lorand.Kalmar@wkfs-frc.local>
2022-04-21 13:30:41 +0300

    Setting transition of background and color to 0.2 seconds

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import CountryList from './components/CountryList/CountryList';
-import classes from './App.module.scss';
-import Header from './components/Layout/Header/Header';
-import SearchBar from './components/UI/SearchBar';
-import { ThemeContext } from './context/theme-context';
-import { useContext } from 'react';
-import RegionFilter from './components/UI/RegionFilter';
+import CountryList from "./components/CountryList/CountryList";
+import classes from "./App.module.scss";
+import Header from "./components/Layout/Header/Header";
+import SearchBar from "./components/UI/SearchBar";
+import { ThemeContext } from "./context/theme-context";
+import { useContext } from "react";
+import RegionFilter from "./components/UI/RegionFilter";
 
 //TODO: Routing
 function App() {
   const themeCtx = useContext(ThemeContext);
 
   const backgroundThemeClass = themeCtx.isLight
-    ? [classes['background'], classes['background--light']].join(' ')
-    : [classes['background'], classes['background--dark']].join(' ');
-  const textThemeClass = themeCtx.isLight
-    ? classes['text--light']
-    : classes['text--dark'];
+    ? [classes["background"], classes["background--light"]].join(" ")
+    : [classes["background"], classes["background--dark"]].join(" ");
+  const textThemeClass = `${classes["text"]} ${
+    themeCtx.isLight ? classes["text--light"] : classes["text--dark"]
+  }`;
 
   return (
-    <div className={[backgroundThemeClass, textThemeClass].join(' ')}>
+    <div className={[backgroundThemeClass, textThemeClass].join(" ")}>
       <Header />
       <SearchBar />
       <RegionFilter />
