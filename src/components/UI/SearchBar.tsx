c353575664d86049d67f751ae5f3a21ca4aa8dd1
Lorand Kalmar <Lorand.Kalmar@wkfs-frc.local>
2022-04-26 13:06:08 +0300

    Fixing search - wasn't able to delete last letter

--- src/components/UI/SearchBar.tsx
+++ src/components/UI/SearchBar.tsx
@@ -1,10 +1,10 @@
-import React, { useContext, useRef } from 'react';
-import { AiOutlineSearch } from 'react-icons/ai';
-import { useDispatch, useSelector } from 'react-redux';
-import { ThemeContext } from '../../context/theme-context';
-import { AppDispatch, RootState } from '../../store';
-import { countryFilterActions } from '../../store/countryFilterSlice';
-import classes from './SearchBar.module.scss';
+import React, { useContext, useRef } from "react";
+import { AiOutlineSearch } from "react-icons/ai";
+import { useDispatch, useSelector } from "react-redux";
+import { ThemeContext } from "../../context/theme-context";
+import { AppDispatch, RootState } from "../../store";
+import { countryFilterActions } from "../../store/countryFilterSlice";
+import classes from "./SearchBar.module.scss";
 
 const SearchBar: React.FC = () => {
   const themeCtx = useContext(ThemeContext);
@@ -14,27 +14,26 @@ const SearchBar: React.FC = () => {
   );
 
   const searchBarThemeClass = themeCtx.isLight
-    ? classes['search-bar--light']
-    : classes['search-bar--dark'];
+    ? classes["search-bar--light"]
+    : classes["search-bar--dark"];
 
   const searchInputThemeClass = themeCtx.isLight
-    ? classes['search-bar__input--light']
-    : classes['search-bar__input--dark'];
+    ? classes["search-bar__input--light"]
+    : classes["search-bar__input--dark"];
 
   const searchHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.value)
-      dispatch(countryFilterActions.setSearchedCountry(e.target.value));
+    dispatch(countryFilterActions.setSearchedCountry(e.target.value));
   };
 
   return (
-    <div className={[classes['search-bar'], searchBarThemeClass].join(' ')}>
-      <AiOutlineSearch className={classes['search-bar__icon']} />
+    <div className={[classes["search-bar"], searchBarThemeClass].join(" ")}>
+      <AiOutlineSearch className={classes["search-bar__icon"]} />
 
       <input
-        className={[classes['search-bar__input'], searchInputThemeClass].join(
-          ' '
+        className={[classes["search-bar__input"], searchInputThemeClass].join(
+          " "
         )}
-        placeholder='Search for a country...'
+        placeholder="Search for a country..."
         value={searchedCountry}
         onChange={searchHandler}
       />
