e089f244f04b40e876e0dd21ad87105bf95adad2
Lorand98 <kalmarlorand@yahoo.com>
2022-05-22 23:07:15 +0300

    Countries filtered based on the current page and the countries/pages

--- src/pages/CountryListPage.tsx
+++ src/pages/CountryListPage.tsx
@@ -1,12 +1,15 @@
-import CountryList from "../components/CountryList/CountryList";
-import Filter from "../components/UI/Filter";
+import { useState } from 'react';
+import CountryList from '../components/CountryList/CountryList';
+import Filter from '../components/UI/Filter';
 
 const CountryListPage: React.FC = () => {
+  const [currentPage, setCurrentPage] = useState(0);
+
   return (
     <>
-      <div style={{ padding: "0 5%" }}>
+      <div style={{ padding: '0 5%' }}>
         <Filter />
-        <CountryList />
+        <CountryList currentPage={currentPage} />
       </div>
     </>
   );
