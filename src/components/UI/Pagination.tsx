11598b3678cc1456c43401dcb9cc9cd02f14aa5c
Lorand98 <kalmarlorand@yahoo.com>
2022-05-24 22:58:38 +0300

    -created JSX from pagination buttons

--- src/components/UI/Pagination.tsx
+++ src/components/UI/Pagination.tsx
@@ -1,7 +1,29 @@
-const Pagination: React.FC<{ onSelectPage: (pageNr: number) => void }> = (
-  onSelectPage
-) => {
-  return <div></div>;
+import classes from '../../sass/components/Pagination.module.scss';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../store';
+import { COUNTRIES_NR_ON_A_PAGE } from '../../constants';
+
+const Pagination: React.FC<{
+  selectedPage: number;
+  onSelectPage: (pageNr: number) => void;
+}> = ({ selectedPage, onSelectPage }) => {
+  const { countries } = useSelector((state: RootState) => state.countries);
+  const totalPages = Math.ceil(countries.length / COUNTRIES_NR_ON_A_PAGE);
+
+  let pageNavigationJSX =
+    selectedPage === 0 ? (
+      <button>Previous page</button>
+    ) : selectedPage === totalPages - 1 ? (
+      <button>Next page</button>
+    ) : (
+      <>
+        {' '}
+        <button>Previous page</button>
+        <button>Next page</button>
+      </>
+    );
+
+  return <div className={classes['pagination']}>{pageNavigationJSX}</div>;
 };
 
 export default Pagination;
