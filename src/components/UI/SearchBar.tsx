7f46bee06f43a01d288a4f29d7d83e53b226e67f
Lorand98 <kalmarlorand@yahoo.com>
2022-05-29 13:03:27 +0300

    Search removing button added to search bar

--- src/components/UI/SearchBar.tsx
+++ src/components/UI/SearchBar.tsx
@@ -1,10 +1,11 @@
-import React, { useContext } from "react";
-import { AiOutlineSearch } from "react-icons/ai";
-import { useDispatch, useSelector } from "react-redux";
-import { ThemeContext } from "../../context/theme-context";
-import { AppDispatch, RootState } from "../../store";
-import { countryFilterActions } from "../../store/countryFilterSlice";
-import classes from "../../sass/components/SearchBar.module.scss";
+import React, { useContext } from 'react';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { TiDeleteOutline } from 'react-icons/ti';
+import { useDispatch, useSelector } from 'react-redux';
+import { ThemeContext } from '../../context/theme-context';
+import { AppDispatch, RootState } from '../../store';
+import { countryFilterActions } from '../../store/countryFilterSlice';
+import classes from '../../sass/components/SearchBar.module.scss';
 
 const SearchBar: React.FC<{ resetPages: () => void }> = ({ resetPages }) => {
   const themeCtx = useContext(ThemeContext);
@@ -14,30 +15,43 @@ const SearchBar: React.FC<{ resetPages: () => void }> = ({ resetPages }) => {
   );
 
   const searchBarThemeClass = themeCtx.isLight
-    ? classes["search-bar--light"]
-    : classes["search-bar--dark"];
+    ? classes['search-bar--light']
+    : classes['search-bar--dark'];
 
   const searchInputThemeClass = themeCtx.isLight
-    ? classes["search-bar__input--light"]
-    : classes["search-bar__input--dark"];
+    ? classes['search-bar__input--light']
+    : classes['search-bar__input--dark'];
 
   const searchHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     dispatch(countryFilterActions.setSearchedCountry(e.target.value));
     resetPages();
   };
 
+  const removeSearchHandler = () => {
+    dispatch(countryFilterActions.setSearchedCountry(''));
+  };
+
   return (
-    <div className={[classes["search-bar"], searchBarThemeClass].join(" ")}>
-      <AiOutlineSearch className={classes["search-bar__icon"]} />
+    <div className={[classes['search-bar'], searchBarThemeClass].join(' ')}>
+      <AiOutlineSearch className={classes['search-bar__icon']} />
 
       <input
-        className={[classes["search-bar__input"], searchInputThemeClass].join(
-          " "
+        className={[classes['search-bar__input'], searchInputThemeClass].join(
+          ' '
         )}
-        placeholder="Search for a country..."
+        placeholder='Search for a country...'
         value={searchedCountry}
         onChange={searchHandler}
       />
+      {
+        <TiDeleteOutline
+          className={[
+            classes['search-bar__icon'],
+            classes['search-bar__icon--x'],
+          ].join(' ')}
+          onClick={removeSearchHandler}
+        />
+      }
     </div>
   );
 };
