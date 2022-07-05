daa0e88bcb6bdd6213279f3d368d6ed89a37c03e
Lorand98 <kalmarlorand@yahoo.com>
2022-07-05 20:13:02 +0300

    Searchbar test finished default render changed with renderWithProviders for the unique store generation

--- src/components/UI/SearchBar.test.tsx
+++ src/components/UI/SearchBar.test.tsx
@@ -2,6 +2,7 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { Provider } from 'react-redux';
 import store from '../../store';
+import { renderWithProviders } from '../../test-utils';
 import SearchBar from './SearchBar';
 
 describe('SearchBar component', () => {
@@ -11,26 +12,26 @@ describe('SearchBar component', () => {
   //     useDispatch: () => mockDispatch,
   //   }));
 
+  const inputText = 'Input Test';
+
   test('renders input text', () => {
-    render(
-      <Provider store={store}>
-        <SearchBar resetPages={() => {}} />
-      </Provider>
-    );
+    renderWithProviders(<SearchBar resetPages={() => {}} />);
 
-    const inputText = 'Input Test';
+    const textbox = screen.getByRole<HTMLInputElement>('textbox');
 
-    userEvent.type(screen.getByRole('textbox'), inputText);
+    userEvent.type(textbox, inputText);
 
-    expect(screen.getByDisplayValue(inputText).id).toBe('searchBarInput');
+    expect(textbox.value).toBe(inputText);
   });
   test('clears text when clicking the clear icon', () => {
-    render(
-      <Provider store={store}>
-        <SearchBar resetPages={() => {}} />
-      </Provider>
-    );
+    renderWithProviders(<SearchBar resetPages={() => {}} />);
+
+    const textbox = screen.getByRole<HTMLInputElement>('textbox');
+
+    userEvent.type(textbox, inputText);
 
     userEvent.click(screen.getByTestId('deleteIcon'));
+
+    expect(textbox.value).toBe('');
   });
 });
